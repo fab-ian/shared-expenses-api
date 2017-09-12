110fbcc80369b003cc932dcdea1e48cc612fe60f
Fabian Kedzierski <grafska@gmail.com>
2017-09-12 16:00:35 +0200

    Implement User controller and add tests

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[show update destroy]
+
+  def index
+    @users = User.all
+    json_response(@users)
+  end
+
+  def show
+    json_response(@user)
+  end
+
+  def create
+    @user = User.create!(user_params)
+    json_response(@user, :created)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def update
+    @user.update(user_params)
+    head :no_content
+  end
+
+  def destroy
+    @user.destroy
+    head :no_content
+  end
+
+  private
+
+  def user_params
+    params.permit(:name)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,96 @@
+RSpec.describe 'UserAPI', type: :request do
+  let!(:users) { create_list(:user, 20) }
+  let(:user_id) { users.first.id }
+
+  describe 'GET /users' do
+    before { get '/users' }
+
+    it 'returns users' do
+      expect(json).not_to be_empty
+      expect(json.size).to eq(20)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET /users/:id' do
+    before { get "/users/#{user_id}" }
+
+    context 'when the record exist' do
+      it 'return the user' do
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(user_id)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the record does not exist' do
+      let(:user_id) { 1000 }
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find User/)
+      end
+    end
+  end
+
+  describe 'POST /users' do
+    let(:valid_attributes) { { name: 'Fabian' } }
+
+    context 'when the request is valid' do
+      before { post '/users', params: valid_attributes }
+
+      it 'create a user' do
+        expect(json['name']).to eq('Fabian')
+      end
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'when the request is invalid' do
+      before { post '/users', params: { name: nil } }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a validation failure message' do
+        expect(response.body)
+          .to match(/Validation failed: Name can't be blank/)
+      end
+    end
+  end
+
+  describe 'PUT /users/:id' do
+    let(:valid_attributes) { { name: 'Paulina' } }
+
+    context 'when the record exist' do
+      before { put "/users/#{user_id}", params: valid_attributes }
+
+      it 'updates the record' do
+        expect(response.body).to be_empty
+      end
+
+      it 'returns status code 204' do
+        expect(response).to have_http_status(204)
+      end
+    end
+  end
+
+  describe 'DELETE /users/:id' do
+    before { delete "/users/#{user_id}" }
+
+    it 'returns status code 204' do
+      expect(response).to have_http_status(204)
+    end
+  end
+end
