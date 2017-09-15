7e64a4a6134a1d636e525db022329ad6aa82deed
Fabian Kedzierski <grafska@gmail.com>
2017-09-15 19:49:01 +0200

    Set associations for ItemUser

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :items
+  has_many :item_users
+  has_many :users, through: :item_users
+
   validates :name, presence: true
   validates :email, presence: true
   validates :password_digest, presence: true
